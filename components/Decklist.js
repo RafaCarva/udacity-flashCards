e074ddec0f1a039eabcf10eb8f144bd78fc78dfc
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2018-02-06 21:47:56 -0200

    Deck.js
    setado o mapeamento da lista inicial para o componente genérico deck.

--- components/Decklist.js
+++ components/Decklist.js
@@ -1,34 +1,37 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { getDecks } from '../utils/helpers'
+import {View,Text,StyleSheet,TouchableOpacity} from 'react-native'
+import {getDecks} from '../utils/helpers'
 
-class Decklist extends React.Component {
-  render() {
+class Decklist extends React.Component{
+    render(){
+        const cards=getDecks()
+        console.log('props de DeckList, ', this.props)
+        return(
+            <View>
+              {/*o erro do map que exige um key se resolve colocando o key em TouchableOpacity*/}
+              {/*oonPress recebe a navegação que veio no this.props */}
+                {cards.map(card=>(
+                    <TouchableOpacity
+                        style={styles.card}
+                        onPress={()=>this.props.navigation.navigate('Deck',{entryId:card.title})}
+                        key={card.title}>
 
-    const cards = getDecks()
-
-    return (
-      <View>
-        {cards.map((card, key) => (
-          <View key={key} style={styles.card}>
-            <Text>{card.title}</Text>
-            <Text>{card.questions.length}
-              {(card.questions.length > 1) ? 'cards' : 'card'}
-            </Text>
-          </View>
-        ))}
-      </View>
-    )
-  }
+                        <Text>{card.title}</Text>
+                        <Text>{card.questions.length} {(card.questions.length>1)?'cards':'card'}</Text>
+                    </TouchableOpacity>
+                ))}
+            </View>
+        )
+    }
 }
 
-const styles = StyleSheet.create({
-  card: {
-    borderBottomWidth: 2,
-    borderBottomColor: '#000',
-    alignItems: 'center',
-    padding: 20
-  }
+const styles=StyleSheet.create({
+    card:{
+        borderBottomWidth:2,
+        borderBottomColor:'blue',
+        alignItems:'center',
+        padding:20
+    }
 })
 
 export default Decklist
