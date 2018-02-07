02577e7529dceeef5535724f234c28ebe6525059
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2018-02-06 22:04:30 -0200

    Estilização do Header e statusBar

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import {TabNavigator,StackNavigator} from 'react-navigation'
+//components
 import DeckList from './components/Decklist'
 import NewDeck from './components/NewDeck'
 import Deck from './components/Deck'
+//utils
+import {Constants} from 'expo'
+
 
 const Tabs=TabNavigator({
     DeckList:{
@@ -30,18 +34,42 @@ const Tabs=TabNavigator({
 //aqui eu seto o mapeamento para as paginas
 const MainNavigator=StackNavigator({
     Home:{
-        screen:Tabs
+        screen:Tabs,
+        navigationOptions:{
+            headerTintColor:'#fff',
+            headerStyle:{
+                backgroundColor:'#5487FF'
+            },
+            title:'FlashCards'
+        }
     },
     Deck:{
-        screen:Deck
+        screen:Deck,
+        navigationOptions:{
+            headerTintColor:'#fff',
+            headerStyle:{
+                backgroundColor:'#5487FF'
+            }
+        }
     }
 })
+
+function UdaciStatusBar({backgroundColor,...props}){
+return(
+    <View style={{backgroundColor:backgroundColor,height:Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    </View>
+)
+}
 
 
 export default class App extends React.Component {
   render() {
     return (
+        <View style={{flex:1}}>
+            <UdaciStatusBar backgroundColor='orange'/>
           <MainNavigator/>
+        </View>
     )
   }
 }
