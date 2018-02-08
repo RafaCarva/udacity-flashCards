9b44c15a8bf914d305973e837a614c666cb704e5
Rafael.Carvalho <rafael.carvalho@assertivasolucoes.com.br>
2018-02-08 16:12:37 -0200

    add Lógica de porcentagem de acertos!

--- components/Finish.js
+++ components/Finish.js
@@ -3,18 +3,41 @@ import {Text,View,StyleSheet,TouchableOpacity} from 'react-native'
 
 class Finish extends React.Component{
     static navigationOptions=({navigation})=>({
-        title:'Quiz Finalizado!'
+        title:'Quiz Done!'
     })
+    state={
+        percentage:0
+    }
+    componentDidMount(){
+        this.setState({
+            percentage:this.props.navigation.state.params.score/this.props.navigation.state.params.maximum * 100
+        })
+    }
+    showCompliment=(percent)=>{
+        if(percent<20){
+            return 'You need to be better!'
+        }
+        if(percent<50){
+            return 'You could be better!'
+        }
+        if(percent<80){
+            return 'That was good!'
+        }
+        else{
+            return 'Genius!'
+        }
+    }
     render(){
-        //console.log(this.props.navigation.state.params)
-
+        console.log(this.props.navigation.state.params)
         return(
-
             <View style={styles.container}>
-                <Text style={{fontSize:24,textAlign:'center'}}>Hello!</Text>
+                <Text style={{fontSize:24,textAlign:'center'}}>Hello, {this.showCompliment(this.state.percentage)}</Text>
                 <Text style={{textAlign:'center'}}> You have scored: </Text>
                 <View style={styles.score}>
                     <Text style={{textAlign:'center',fontSize:64}}>{this.props.navigation.state.params.score} / {this.props.navigation.state.params.maximum}</Text>
+                </View>
+                <View>
+                    <Text style={{textAlign:'center'}}>That means you scored {this.state.percentage} %</Text>
                 </View>
                 <TouchableOpacity
                     onPress={()=>this.props.navigation.navigate('Quiz',{card:this.props.navigation.state.params.card})}
@@ -22,14 +45,13 @@ class Finish extends React.Component{
                 >
                     <Text style={styles.buttonText}>Start Over</Text>
                 </TouchableOpacity>
-
                 <TouchableOpacity
                     onPress={()=>this.props.navigation.navigate('Deck',{card:this.props.navigation.state.params.card,count:this.props.navigation.state.params.count})}
                     style={[styles.button,{backgroundColor:'green'}]}
                 >
                     <Text style={styles.buttonText}>Go to {this.props.navigation.state.params.card}</Text>
                 </TouchableOpacity>
-</View>
+            </View>
         )
     }
 }
@@ -39,21 +61,21 @@ const styles=StyleSheet.create({
         padding:20
     },
     score:{
-        
+
     },
     button:{
-                padding: 10,
-                paddingLeft: 30,
-                paddingRight: 30,
-                height: 45,
-                borderRadius: 2,
-                justifyContent: 'center',
-                alignItems: 'center',
-                margin:10
-            },
-            buttonText:{
-                color:'white'
-            }
+        padding: 10,
+        paddingLeft: 30,
+        paddingRight: 30,
+        height: 45,
+        borderRadius: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin:10
+    },
+    buttonText:{
+        color:'white'
+    }
 })
 
 export default Finish
